b0fb0abc870ad1aedc9e588f166a4bce894027d9
Daniel Ariza <danielariza@csu.fullerton.edu>
2021-11-12 22:38:38 -0800

    some more work on url shortener
    Another day another bit of work

--- Scripts/config/urlshortener/urls.py
+++ Scripts/config/urlshortener/urls.py
@@ -2,11 +2,12 @@
 URLs for shorterner app urlshortener/urls.py
 '''
 from django.urls import path
-from .views import home_view
+from .views import home_view, redirect_url_view
 
 appname = "shorterner"
 
 urlpatterns = [
 
-	path("",home_view, name = "home")
+	path('',home_view, name = 'home'),
+	path('<str.shortened_part>', redirect_url_view, name='redirect'),
 ]
